3ec1dbb922200244b38324ec51ee7e305537e1be
bosun36 <sviridenko@3davinci.ru>
2026-06-06 14:44:56 +0300

    Исправлены пространства имён spatie/laravel-activitylog v5
    LogsActivity: Traits → Models\Concerns, LogOptions: корневой → Support

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,8 +11,8 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Spatie\Activitylog\LogOptions;
-use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\Models\Concerns\LogsActivity;
+use Spatie\Activitylog\Support\LogOptions;
 use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['name', 'email', 'password', 'city', 'admin_notes', 'role'])]
